0d63a9e4d9b4f69f74fb3e8e05de5f921105ec58
cwelke <cwelke@cern.ch>
2013-10-14 15:07:46 -0700

    uploading directory setup script

--- runAllPlots.sh
+++ runAllPlots.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+currentdate="141013"
+
+if [ -d ~/Desktop/ZH_Plots/$currentdate/ ]; then
+	echo "Directory ~/Desktop/ZH_Plots/$currentdate/ Already exists! making plots."
+else
+	echo "Directory ~/Desktop/ZH_Plots/$currentdate/ does not exist! Creating now."
+	mkdir ~/Desktop/ZH_Plots/$currentdate/{data_bg{,/single_events},single_events}
+fi
 
 scp uaf-7:/home/users/cwelke/Analysis/Spring2013/comparisonlooper/photon_template_MET_data_2jetbins_all.root .
 
